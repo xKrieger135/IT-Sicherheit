52d643086cbfad3a25168b866fdaf59e220850eb
Blackgen <jandb07@gmx.de>
2015-12-08 10:26:21 +0100

    Add First RSA implementation

---
+++ it-security/src/main/java/RSA/CommandLineRunner.java
@@ -0,0 +1,24 @@
+package RSA;
+
+import java.io.File;
+
+/**
+ * Created by JanDennis on 25.11.2015.
+ */
+public class CommandLineRunner {
+    private static File privateKeyFile;
+    private static File publicKeyFile;
+
+    public static void main(String[] args) {
+        if (args.length!=1) {
+            System.out.println("Wrong Parameter count!");
+            return;
+        }
+        String name = args[0];
+        RSACrypt RSA = new RSACrypt();
+
+        privateKeyFile = new File(name+".prv");
+        publicKeyFile = new File(name+".pub");
+
+    }
+}
---
+++ it-security/src/main/java/RSA/RSACrypt.java
@@ -0,0 +1,35 @@
+package RSA;
+
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by JanDennis on 25.11.2015.
+ */
+public class RSACrypt {
+    Key publicKey;
+    Key privateKey;
+
+    public RSACrypt() {
+        KeyPairGenerator kpg = null;
+        try {
+            kpg = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        kpg.initialize(2048);
+        KeyPair kp = kpg.genKeyPair();
+        publicKey = kp.getPublic();
+        privateKey = kp.getPrivate();
+    }
+
+    public Key getPrivateKey() {
+        return privateKey;
+    }
+
+    public Key getPublicKey() {
+        return publicKey;
+    }
+}
