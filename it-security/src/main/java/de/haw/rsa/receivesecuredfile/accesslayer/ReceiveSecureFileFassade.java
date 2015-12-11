416f7e65bdd18baa505e0e8c33561b54be8681c7
Blackgen <jandb07@gmx.de>
2015-12-11 23:50:30 +0100

    Add plain ReceiveSecureFile project structure

---
+++ it-security/src/main/java/de/haw/rsa/receivesecuredfile/accesslayer/ReceiveSecureFileFassade.java
@@ -0,0 +1,7 @@
+package de.haw.rsa.receivesecuredfile.accesslayer;
+
+/**
+ * Created by JanDennis on 11.12.2015.
+ */
+public class ReceiveSecureFileFassade {
+}
---
+++ it-security/src/main/java/de/haw/rsa/receivesecuredfile/businesslogiclayer/ReceiveSecureFileBusinessLogic.java
@@ -0,0 +1,7 @@
+package de.haw.rsa.receivesecuredfile.businesslogiclayer;
+
+/**
+ * Created by JanDennis on 11.12.2015.
+ */
+public class ReceiveSecureFileBusinessLogic {
+}
