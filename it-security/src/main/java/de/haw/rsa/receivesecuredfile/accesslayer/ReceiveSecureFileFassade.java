2ac05f12c65ebac235552e9a03fa7d0cd0e14590
Blackgen <jandb07@gmx.de>
2015-12-12 14:24:36 +0100

    some changes in RSF

--- it-security/src/main/java/de/haw/rsa/receivesecuredfile/accesslayer/ReceiveSecureFileFassade.java
+++ it-security/src/main/java/de/haw/rsa/receivesecuredfile/accesslayer/ReceiveSecureFileFassade.java
@@ -1,7 +1,25 @@
 package de.haw.rsa.receivesecuredfile.accesslayer;
 
+import de.haw.rsa.receivesecuredfile.accesslayer.interfaces.IReceiveSecureFile;
+import de.haw.rsa.receivesecuredfile.businesslogiclayer.ReceiveSecureFileBusinessLogic;
+import de.haw.rsa.rsaadapter.adapter.RSAKeyReaderAdapter;
+
+import java.io.File;
+
 /**
  * Created by JanDennis on 11.12.2015.
  */
-public class ReceiveSecureFileFassade {
+public class ReceiveSecureFileFassade implements IReceiveSecureFile {
+    ReceiveSecureFileBusinessLogic businessLogic = null;
+    RSAKeyReaderAdapter keyReaderAdapter = null;
+
+    public ReceiveSecureFileFassade() {
+        businessLogic = new ReceiveSecureFileBusinessLogic();
+        keyReaderAdapter = new RSAKeyReaderAdapter();
+    }
+
+    @Override
+    public File decryptFileWithAES(File inputFile) {
+        return null;
+    }
 }
--- it-security/src/main/java/de/haw/rsa/receivesecuredfile/accesslayer/interfaces/IReceiveSecureFile.java
+++ it-security/src/main/java/de/haw/rsa/receivesecuredfile/accesslayer/interfaces/IReceiveSecureFile.java
@@ -1,7 +1,11 @@
 package de.haw.rsa.receivesecuredfile.accesslayer.interfaces;
 
+import java.io.File;
+
 /**
  * Created by JanDennis on 11.12.2015.
  */
 public interface IReceiveSecureFile {
+
+    File decryptFileWithAES(File inputFile);
 }
